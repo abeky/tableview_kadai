6c4e90dbedf4b416250359ba425cfadc63208fbe
abeky <kazuma.abe01@gmail.com>
2018-06-08 20:11:53 +0900

    Initial Commit

---
+++ tableview_kadai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableview_kadai
+//
+//  Created by 阿部一真 on 2018/06/08.
+//  Copyright © 2018年 阿部一真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
